74149cc8d7ea3001dad33f0ad250e9f1186c7947
Ausianovich <49125846+Ausianovich@users.noreply.github.com>
2022-06-29 16:33:51 +0400

    Add Sample and update version to 1.0.2

---
+++ Sample/TestPodSample/TestPodSample/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TestPodSample
+//
+//  Created by Kanstantsin Ausianovich on 29.06.22.
+//
+
+import UIKit
+import CocoaPodsTest_Ausianovich
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        //Example
+        let array = [1, 2, 3]
+        let element = array.element(at: 2)
+    }
+
+
+}
+
